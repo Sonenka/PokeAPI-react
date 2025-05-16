9cc1a33c86a9383867680c472024f436e53b2d22
Sonya <sonenkkaa@gmail.com>
2025-05-16 20:53:01 +0300

    trying to deploy

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/pokeapi-react/', // имя репозитория на GitHub
+  base: '/PokeAPI-react/', // имя репозитория на GitHub
 });
